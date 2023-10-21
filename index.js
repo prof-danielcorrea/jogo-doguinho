004720eddcc8de249d6695515d9abd8abb049375
prof-danielcorrea <danielprofaetec@gmail.com>
2023-10-21 20:31:25 -0300

    jogo-doguinho

---
+++ index.js
@@ -0,0 +1,31 @@
+ function escolherCorMarrom() {
+     let cores = document.getElementsByClassName('dog');
+     let animais = cores [0];
+     animais.style.backgroundColor = 'goldenrod';
+ }
+
+ function escolherCorAmarela() {
+    let cores = document.getElementsByClassName('dog');
+    let animais = cores [0];
+    animais.style.backgroundColor = 'yellow';
+}
+
+function escolherCorVioleta() {
+    let cores = document.getElementsByClassName('dog');
+    let animais = cores [0];
+    animais.style.backgroundColor = 'blueviolet';
+}
+
+function escolherCorVerde() {
+    let cores = document.getElementsByClassName('dog');
+    let animais = cores [0];
+    animais.style.backgroundColor = 'yellowgreen';
+}
+
+function escolherCorBranca() {
+    let cores = document.getElementsByClassName('dog');
+    let animais = cores [0];
+    animais.style.backgroundColor = 'white';
+}
+
+
